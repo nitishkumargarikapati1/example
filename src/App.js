cbad02f7c65c34b700762d078f2278bd8a5180fc
nitishkumargarikapati1 <nitishkumargarikapati.1@gmail.com>
2022-07-26 12:26:51 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+ import './App.css';
+
+import Family from './context/Family';
+import {FamilyContext} from './context/FamilyContext';
+
+const familySecret = {
+  familyName: 'The Smiths',
+  onlyParentCanSee: () => {
+    return `The Smiths are the best!`;
+  },
+  onlyGrandChildrenShouldKnow: () => {
+    return `They are the best!`;
+  }
+}
+
+function App() {
+  return (
+   
+    <FamilyContext.Provider value={familySecret}>
+      <Family />
+    </FamilyContext.Provider>      
+  );
+}
+
+export default App;
